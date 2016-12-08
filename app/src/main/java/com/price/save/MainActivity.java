6b0683d7289dad274c021fe82c4888e2ccc8ba95
price <inteli5760@163.com>
2016-12-08 21:20:16 +0800

    save firt

---
+++ app/src/main/java/com/price/save/MainActivity.java
@@ -0,0 +1,13 @@
+package com.price.save;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
