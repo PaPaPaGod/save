d834492b5d78c25974934caf7c679ae718d2cf1e
price <inteli5760@163.com>
2016-12-08 23:40:29 +0800

    test

---
+++ app/src/main/java/com/price/save/Config.java
@@ -0,0 +1,9 @@
+package com.price.save;
+
+/**
+ * Created by price on 2016/12/8.
+ */
+
+public class Config {
+//    public static final
+}
---
+++ app/src/main/java/com/price/save/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.price.save;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by price on 2016/12/8.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getVersionCode();
+    }
+
+
+    private void getVersionCode() {
+        PackageManager packageManager = getPackageManager();
+        try {
+            PackageInfo info = packageManager.getPackageInfo(getPackageName(),0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
